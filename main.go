0402d0cd28b0f35a4d70d0b4f15a7dad8214b88d
tatsiy <36859224+JekaTatsiy@users.noreply.github.com>
2022-07-27 10:42:22 +0300

    Add files via upload

--- main.go
+++ main.go
@@ -3,15 +3,21 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
 
 func main() {
+	port := 3000
+	if p := os.Getenv("PORT"); len(p) != 0 {
+		port, _ = strconv.Atoi(p)
+	}
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { w.Write([]byte(r.RemoteAddr)) }).Methods(http.MethodGet)
-	s := http.Server{Addr: "0.0.0.0:1000", Handler: r}
+	s := http.Server{Addr: fmt.Sprintf("0.0.0.0:%d", port), Handler: r}
 
 	fmt.Println(s.ListenAndServe())
 }
